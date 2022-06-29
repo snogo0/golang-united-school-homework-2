d17c114d6d1092e8538e8f0662f744f81947196d
Alex M <snogo@e1.ru>
2022-06-29 20:36:31 +0500

    Implement square calculation
    Valid for circles, squares and equilateral triangles only.

--- solution.go
+++ solution.go
@@ -1,12 +1,28 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+type Sides int
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+const SidesTriangle = 3
+const SidesSquare = 4
+const SidesCircle = 0
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum Sides) float64 {
+	switch sidesNum {
+	case SidesCircle:
+		return sideLen * sideLen * math.Pi
+	case SidesTriangle:
+		return sideLen * sideLen * math.Sqrt(3) / 4
+	case SidesSquare:
+		return sideLen * sideLen
+	default:
+		return 0
+	}
 }
